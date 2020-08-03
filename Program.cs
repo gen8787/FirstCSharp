314d133d98bb1a5f2d2809f4d7a18e2d27b5bb36
Gary <66178774+gen8787@users.noreply.github.com>
2020-08-03 16:16:41 -0700

    en espanol

--- Program.cs
+++ Program.cs
@@ -7,6 +7,7 @@ namespace FirstCSharp
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.WriteLine("Hola Mundo!");
         }
     }
 }
